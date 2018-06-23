4d1072e7c6b3c8de60f380d7611c9c73e6b71889
PratikshaMaheshwari <pratikshamaheshwari04@gmail.com>
2018-06-23 17:30:55 +0530

    Second commit

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use((req, res, next) => {
   console.log(log);
   fs.appendFile('server.log', log + '\n', (err) => {
       if(err){
-        console.log('Unable to connect');
+        console.log('Unable to connect to the server');
       }
   });
   next();
